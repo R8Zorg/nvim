697881531ed9435153b110accedb084a3f64f7f0
Hak1e <hakie.notiko@gmail.com>
2025-04-23 23:08:11 +0400

    Removed copying to buffer on line deleting. Edited comments position

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,12 +7,10 @@ vim.keymap.set("n", "<C-s>", "<cmd>:w<CR>")
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<C-l>", ":BufferLineCloseOthers<CR>")
--- Quit
-vim.keymap.set("n", "<C-q>", "<cmd>:qa<CR>")
--- clear highlights
-vim.keymap.set('n', '<Esc>', ':noh<CR>', opts)
--- delete single character without copying into register
-vim.keymap.set('n', 'x', '"_x', opts)
+vim.keymap.set("n", "<C-q>", "<cmd>:qa<CR>") -- Quit
+vim.keymap.set('n', '<C-a>', 'ggVG', opts) -- Select all
+vim.keymap.set('n', '<Esc>', ':noh<CR>', opts) -- clear highlights
+vim.keymap.set('n', 'x', '"_x', opts) -- delete single character without copying into register
 
 -- Resize with arrows
 vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
@@ -30,7 +28,7 @@ vim.keymap.set('n', '<leader>nb', '<cmd> enew <CR>', opts) -- new buffer
 vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
 vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+vim.keymap.set('n', '<leader>cw', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
 vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
@@ -44,8 +42,7 @@ vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
 vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
 vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
 
--- Toggle line wrapping
-vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts) -- Toggle line wrapping
 
--- Keep last yanked when pasting
-vim.keymap.set('v', 'p', '"_dP', opts)
+vim.keymap.set('v', 'p', '"_dP', opts) -- Keep last yanked when pasting
+vim.keymap.set('n', 'dd', '"_dd', opts) -- Do not copy deleted line
